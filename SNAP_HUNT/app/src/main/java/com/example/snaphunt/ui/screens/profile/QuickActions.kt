9cf11286e4e529548da4ccf1c415133f44bc72b4
Giulia <giulia.albertini3@studio.unibo.it>
2026-06-26 17:02:33 +0200

    Fixed useless imports

--- SNAP_HUNT/app/src/main/java/com/example/snaphunt/ui/screens/profile/QuickActions.kt
+++ SNAP_HUNT/app/src/main/java/com/example/snaphunt/ui/screens/profile/QuickActions.kt
@@ -2,7 +2,6 @@ package com.example.snaphunt.ui.screens.profile
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.snaphunt.SnapHuntRoute
-import com.example.snaphunt.data.user.UserLogInData
 import com.example.snaphunt.presentation.sign_in.AuthViewModel
 import com.example.snaphunt.ui.components.ActionButton
 import com.example.snaphunt.ui.components.ExpandableSection
